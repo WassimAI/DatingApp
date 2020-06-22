d6f4bb72cd18a2f79f102f6d225ce2e870fc594b
wassimAI - Wassim Ibrahim <40058827+WassimInstaclass@users.noreply.github.com>
2020-06-22 19:15:38 +0400

    End of part 7

---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,23 @@
+import {Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+/* Ordering is important here */
+export const appRoutes: Routes = [
+    { path: '', component: HomeComponent},
+    {
+        path: '', /*what is this empty string? It simply like nothing, so the route will
+        be /members, if we put xyz in here, then the route will be //localhost:etc/xyz/members */
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent},
+            { path: 'messages', component: MessagesComponent},
+            { path: 'lists', component: ListsComponent}
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full'}
+];
