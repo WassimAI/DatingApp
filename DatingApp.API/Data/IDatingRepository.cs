dc15c71b0e009678beac62576fc7a8b7bd1d7236
wassimAI - Wassim Ibrahim <40058827+WassimInstaclass@users.noreply.github.com>
2020-06-23 17:14:34 +0400

    Part 8, Extending my API

---
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public interface IDatingRepository
+    {
+         void Add<T>(T entity) where T: class;//we can use this same method for two add types (user and photo)
+         void Delete<T>(T entity) where T: class;
+         Task<bool> SaveAll();
+         Task<IEnumerable<User>> GetUsers();
+         Task<User> GetUser(int id);
+    }
+}
